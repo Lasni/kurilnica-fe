d4d82ac42d71e15adffb0eff2d6743651ad3933c
Lasni <grega.lasnibat@gmail.com>
2022-12-03 12:53:11 +0100

    Added chakra and set up default theme

---
+++ src/chakra/theme.ts
@@ -0,0 +1,28 @@
+// 1. Import `extendTheme`
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme(
+  { config },
+  {
+    colors: {
+      brand: {
+        100: "#3d84f7",
+        // ...
+        // 900: "#1a202c",
+      },
+    },
+    styles: {
+      global: () => ({
+        body: {
+          bg: "whiteAlpha.200",
+        },
+      }),
+    },
+  }
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { theme } from "../chakra/theme";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ChakraProvider theme={theme}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
   );
 }
