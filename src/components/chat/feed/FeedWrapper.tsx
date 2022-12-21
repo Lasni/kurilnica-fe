dabdebfd3c89dda4c3c93132c000d60040f2296a
Lasni <grega.lasnibat@gmail.com>
2022-12-21 13:29:17 +0100

    Displaying conversationId inside of FeedWrapper

--- src/components/chat/feed/FeedWrapper.tsx
+++ src/components/chat/feed/FeedWrapper.tsx
@@ -1,4 +1,6 @@
+import { Flex } from "@chakra-ui/react";
 import { Session } from "next-auth";
+import { useRouter } from "next/router";
 
 interface FeedWrapperProps {
   session: Session;
@@ -7,7 +9,17 @@ interface FeedWrapperProps {
 const FeedWrapper: React.FunctionComponent<FeedWrapperProps> = ({
   session,
 }) => {
-  return <>Feed wrapper</>;
+  const router = useRouter();
+  const { conversationId } = router.query;
+  return (
+    <Flex
+      width="100%"
+      direction="column"
+      display={{ base: conversationId ? "flex" : "none", md: "flex" }}
+    >
+      {conversationId ? <Flex>{conversationId}</Flex> : <>no convo selected</>}
+    </Flex>
+  );
 };
 
 export default FeedWrapper;
