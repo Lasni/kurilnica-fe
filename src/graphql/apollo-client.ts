04b39f2357c59d2e583d9c7057fe1a597b8e8c5f
Lasni <grega.lasnibat@gmail.com>
2022-12-23 09:43:32 +0100

    Apollo websocket setup

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -1,4 +1,9 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+import { createClient } from "graphql-ws";
+import { getSession } from "next-auth/react";
 
 // graphql endpoint for sending http requests to our server
 const httpLink = new HttpLink({
@@ -6,8 +11,39 @@ const httpLink = new HttpLink({
   credentials: "include",
 });
 
+/**
+ * make sure that we're in the browser, not the nextjs server
+ */
+const wsLink =
+  typeof window !== "undefined"
+    ? new GraphQLWsLink(
+        createClient({
+          url: "ws://localhost:4000/graphql/subscriptions",
+          connectionParams: async () => ({
+            session: await getSession(),
+          }),
+        })
+      )
+    : null;
+
+const splitLink =
+  typeof window !== "undefined" && wsLink !== null
+    ? split(
+        ({ query }) => {
+          const definition = getMainDefinition(query);
+
+          return (
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
+          );
+        },
+        wsLink,
+        httpLink
+      )
+    : httpLink;
+
 // apollo client instance
 export const apolloClient = new ApolloClient({
-  link: httpLink,
+  link: splitLink,
   cache: new InMemoryCache(), // built-in caching
 });
