Lasni/kurilnica-fe

commit 4ede85b453a7dd03beb5fd459a2e179ba4effeab

Working on adding users via updateConversation. Introduced latestMessage bug.

src/components/chat/conversations/ConversationsWrapper.tsx
@@ -64,6 +64,7 @@ const ConversationsWrapper: React.FunctionComponent<
           conversationUpdated: {
             conversation: updatedConversation,
             removedUserIds,
+            addedUserIds,
           },
         } = conversationUpdatedSubscriptionData;
 
@@ -71,37 +72,68 @@ const ConversationsWrapper: React.FunctionComponent<
           updatedConversation;
 
         /**
-         * Check if user is being removed
+         * User being removed
          */
         if (removedUserIds && removedUserIds.length > 0) {
+          // console.log("removedUserIds: ", removedUserIds);
+          // console.log("userId: ", userId);
           const isBeingRemoved = removedUserIds.find((id) => id === userId);
-          if (isBeingRemoved) {
-            const conversationsData =
-              client.readQuery<ConversationsQueryOutput>({
-                query: conversationOperations.Queries.conversations,
-              });
+          if (!isBeingRemoved) return;
+          const conversationsData = client.readQuery<ConversationsQueryOutput>({
+            query: conversationOperations.Queries.conversations,
+          });
+          if (!conversationsData) return;
+          console.log("conversationsData", conversationsData);
+          const filteredConversations = conversationsData.conversations.filter(
+            (c) => c.id !== updatedConversationId
+          );
+          client.writeQuery<ConversationsQueryOutput>({
+            query: conversationOperations.Queries.conversations,
+            data: {
+              conversations: filteredConversations,
+            },
+          });
+          // Redirect to home after cache updates
+          if (conversationId === updatedConversationId) {
+            router.replace("");
+          }
+          // Early return - no more updates required
+        }
 
-            if (!conversationsData) return;
+        /**
+         * Users being added
+         */
+        if (addedUserIds && addedUserIds.length > 0) {
+          console.log("userId: ", userId);
+          console.log("addedUserIds: ", addedUserIds);
 
-            const filteredConversations =
-              conversationsData.conversations.filter(
-                (c) => c.id !== updatedConversationId
-              );
+          const isBeingAdded = addedUserIds.find((id) => id === userId);
+          if (!isBeingAdded) return;
 
+          console.log("updatedConversation: ", updatedConversation);
+
+          const conversationsData = client.readQuery<ConversationsQueryOutput>({
+            query: conversationOperations.Queries.conversations,
+          });
+
+          if (conversationsData) {
             client.writeQuery<ConversationsQueryOutput>({
               query: conversationOperations.Queries.conversations,
               data: {
-                conversations: filteredConversations,
+                conversations: [
+                  updatedConversation,
+                  ...conversationsData.conversations,
+                ],
               },
             });
-
-            // Redirect to home after cache updates
-            if (conversationId === updatedConversationId) {
-              router.replace("");
-            }
-
-            // Early return - no more updates required
-            return;
+          } else {
+            console.log("else");
+            client.writeQuery<ConversationsQueryOutput>({
+              query: conversationOperations.Queries.conversations,
+              data: {
+                conversations: [updatedConversation],
+              },
+            });
           }
         }
 

src/components/chat/conversations/modal/ConversationModal.tsx
@@ -168,7 +168,6 @@ export const ConversationModal = ({
       const { data } = await updateConversation({
         variables: { conversationId, participantIds },
       });
-
       console.log("handleUpdateConversation data: ", data);
     } catch (error: any) {
       toast.error("Failed to update the conversation");

src/graphql/operations/conversation.ts
@@ -104,6 +104,7 @@ const conversationOperations = {
             ${conversationFields}
           }
           removedUserIds
+          addedUserIds
         }
       }
     `,

src/interfaces/graphqlInterfaces.ts
@@ -102,6 +102,7 @@ export interface ConversationUpdatedSubscriptionOutput {
   conversationUpdated: {
     conversation: ConversationPopulated;
     removedUserIds: Array<string>;
+    addedUserIds: Array<string>;
   };
 }
 

commit 3317b63d876f8284d288e04f597dc48d4e03b121

Created test-branch02 from 24e46f00ca3cd81d885ee426545bfdf4ce145c49 Working on figuring out how bug was introduced in 4ede85b453a7dd03beb5fd459a2e179ba4effeab

src/components/chat/conversations/ConversationsWrapper.tsx
@@ -51,12 +51,18 @@ const ConversationsWrapper: React.FunctionComponent<
     MarkConversationAsReadUseMutationInput
   >(conversationOperations.Mutations.markConversationAsRead);
 
+  //! Created test-branch02 where new message updates to conversationItems still works. Will work from here
+  //! Note - here when a new message is sent this subscription fires, whereas on the buggy commit it doesn't
   // conversationUpdated subscription
   useSubscription<ConversationUpdatedSubscriptionOutput, null>(
     conversationOperations.Subscriptions.conversationUpdated,
     {
       onData: ({ client, data }) => {
         const { data: conversationUpdatedSubscriptionData } = data;
+        console.log(
+          "conversationUpdatedSubscriptionData",
+          conversationUpdatedSubscriptionData
+        );
 
         if (!conversationUpdatedSubscriptionData) return;
 